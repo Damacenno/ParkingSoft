c15956c437c57348a2bc0b18d0ecd97be0491389
Caio Gomes <caiogomesteodoro21@gmail.com>
2024-03-03 21:59:30 -0300

    att codigo

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -13,31 +13,19 @@ $registros = listar_registros($conn, 1);
                 <th scope="col">Modelo</th>
                 <th scope="col">Vaga</th>
                 <th style=" width: 20%;" scope="col">Entrada</th>
-                <th scope="col">Ação</th>
-
-            </tr>
-        </thead>
-        <tbody id="tbody">
-        </tbody>
-    </table>
-    <table id="tabelaCarros_dashboard" class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th scope="col">Placa</th>
-                <th scope="col">Modelo</th>
-                <th scope="col">Vaga</th>
-                <th style=" width: 20%;" scope="col">Entrada</th>
-                <th scope="col">Ação</th>
+                <th scope="col">Saída</th>
             </tr>
         </thead>
         <tbody id="tbody">
             <?php if ($registros) {
                 foreach ($registros as $registro) {
-                    echo "<td>" . $registro['id_registro'] . "<td>";
-                    echo "<td>" . $registro[''] . "<td>";
-                    echo "<td>" . $registro[''] . "<td>";
-                    echo "<td>" . $registro[''] . "<td>";
-                    echo "<td>" . $registro[''] . "<td>";
+                    echo "<tr>";
+                    echo "<td>" . $registro['placa_carro'] . "</td>";
+                    echo "<td>" . $registro['modelo_carro'] . "</td>";
+                    echo "<td>" . $registro['vaga_carro'] . "</td>";
+                    echo "<td>" . $registro['entrada_carro'] . "</td>";
+                    echo "<td>" . $registro['saida_carro'] . "</td>";
+                    echo "</tr>";
                 }
                 ?>
 
