742be4577ab9711fecf81eefc018da60fd919d0f
CAIO GOMES TEODORO <114540000+CaioGomest@users.noreply.github.com>
2024-03-11 13:50:08 -0300

    att grafico

---
+++ Function_grafico.php
@@ -0,0 +1,30 @@
+<?php
+require("Function_registros.php");
+
+$registros = listar_registros($conn, 1);
+
+if ($registros) 
+{
+ 
+
+    $registros_vagas = array();
+    foreach ($registros as $registro) {
+        // Extrai a data de entrada do registro e converte para milissegundos
+        $valorX = $registro['entrada_auto'] * 1000; // Multiplica por 1000 para converter para milissegundos
+
+        // Extrai o valor Y do registro (substitua 'valorY' pelo nome do campo correto)
+        $valorY = 1; // Substitua 'valorY' pelo nome do campo correto
+
+        $registro_vaga = array(date('d/m/Y H:i', $registro['entrada_auto'] / 1000), $valorY); // Inverte a ordem dos valores
+        $registros_vagas[] = $registro_vaga;
+    }
+
+    $dados = Array(
+        Array(
+            "name" => 'estacionados',
+            "data" => $registros_vagas
+        )
+    );
+
+    echo json_encode($dados); // Alterado para echo em vez de return}
+}
